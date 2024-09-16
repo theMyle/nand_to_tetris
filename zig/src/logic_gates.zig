3a712bb20ed4b39b58bb85f39a4f5879963da389
John Kyle Desamparo <99082612+theMyle@users.noreply.github.com>
2024-09-16 20:12:50 +0800

    I deleted all of my fucking progress with git reset peace of shit

--- zig/src/logic_gates.zig
+++ zig/src/logic_gates.zig
@@ -1,18 +1,32 @@
 const std = @import("std");
 
-// BASE NAND GATE
-pub fn Nand(a: u2, b: u2) u2 {
+/// Base `NAND` gate
+///
+/// Used to implement all other gates.
+pub fn Nand(a: u1, b: u1) u1 {
     switch (a & b) {
         1 => return 0,
         else => return 1,
     }
 }
 
-pub fn Not(in: u2) u2 {
+/// `NOT` gate
+///
+/// Used to invert input
+///
+///     Not(1) -> 0
+///     Not(0) -> 1
+pub fn Not(in: u1) u1 {
     return Nand(in, in);
 }
 
-pub fn And(a: u2, b: u2) u2 {
+/// `AND` gate
+///
+/// Returns true only if both of the inputs are true
+///
+///     And(1,1) -> 1
+///     else     -> 0
+pub fn And(a: u1, b: u1) u1 {
     // Nand = Not (a and b)
     // We can cancel out the not by using another not:
     // Not ( Not (a and b)) = a and b
@@ -20,11 +34,24 @@ pub fn And(a: u2, b: u2) u2 {
     return Not(nand);
 }
 
-pub fn Or(a: u2, b: u2) u2 {
+/// `OR` gate
+///
+/// Returns true if atleast one of the input is true
+///
+///     Or(1,0) -> 1
+///     Or(0,1) -> 1
+///     Or(1,1) -> 1
+pub fn Or(a: u1, b: u1) u1 {
     return Nand(Not(a), Not(b));
 }
 
-pub fn Xor(a: u2, b: u2) u2 {
+/// `XOR` - Exclusive OR gate
+///
+/// Returns true if and only if one of the ouput is true
+///
+///     Xor(1,0) -> 1
+///     Xor(0,1) -> 1
+pub fn Xor(a: u1, b: u1) u1 {
     const notA = Not(a);
     const notB = Not(b);
     return Or(And(notA, b), And(a, notB));
